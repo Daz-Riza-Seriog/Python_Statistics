91631748172ce0cd2b07b18b514bc1bace51e972
Sergio Andrés Díaz Ariza <69307962+CHEJIN95@users.noreply.github.com>
2021-02-21 14:46:18 -0500

    Use of MatPlotLib with numpy, using a plt.plot, plt.label, plt.ylabel, plt.title, plt.legenmd, plt show

---
+++ Univariate Data/Python for Analysis of Univariate Data/MatplotlLib.py
@@ -0,0 +1,54 @@
+################################################################
+# Matplotlib is a plotting library. In this section            #
+# give a brief introduction to the matplotlib.pyplot module.   #
+################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Compute the x and y coordinates for points on a sine curve
+x = np.arange(0, 3 * np.pi, 0.1)
+y = np.sin(x)
+
+# Plot the points using matplotlib
+plt.plot(x, y)
+plt.show()  # You must call plt.show() to make graphics appear.
+
+# Compute the x and y coordinates for points on sine and cosine curves
+x = np.arange(0, 3 * np.pi, 0.1)
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+
+# Plot the points using matplotlib
+plt.plot(x, y_sin)
+plt.plot(x, y_cos)
+plt.xlabel('x axis label')
+plt.ylabel('y axis label')
+plt.title('Sine and Cosine')
+plt.legend(['Sine', 'Cosine'])
+plt.show()
+
+############################################
+#           Subplots                       #
+############################################
+
+# Compute the x and y coordinates for points on sine and cosine curves
+x = np.arange(0, 3 * np.pi, 0.1)
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+
+# Set up a subplot grid that has height 2 and width 1,
+# and set the first such subplot as active.
+plt.subplot(2, 1, 1)
+
+# Make the first plot
+plt.plot(x, y_sin)
+plt.title('Sine')
+
+# Set the second subplot as active, and make the second plot.
+plt.subplot(2, 1, 2)
+plt.plot(x, y_cos)
+plt.title('Cosine')
+
+# Show the figure.
+plt.show()
